61cafcb3e9714d0eb118e744d13962c0cddde175
SylviaGit4 <torchicky139@gmail.com>
2025-02-28 11:50:00 +0000

    Working on JS

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-console.log(":3");
+console.log(":3"); // logging (printing) a message to the console //
+
+let number = 17; // declaring a variable //
+
+const lang = "JavaScript"; // declaring a constant //
+
+if (number === 17) {
+  // 3rd = means ensuring it's an integer //
+  console.log("Wow, that is the number 17"); // executing a conditional (if) statement //
+}
+
+alert("THIS IS JAVASCRIPT"); // shows a popup message //
+
+const answer = confirm("Do you really want to see javascript?"); // shows a popup with OK and Cancel buttons //
+
+console.log(answer); // logging the result of the confirm() function //
+
+let opinion = prompt("Do you like JavaScript?", "Yes"); // shows a popup with a text input field //
+
+console.log(opinion); // logging the result of the prompt() function //
