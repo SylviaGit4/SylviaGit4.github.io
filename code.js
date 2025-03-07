f5c817b5033d18c1091ee4e6a0e253f46ec192a4
SylviaGit4 <torchicky139@gmail.com>
2025-03-07 12:02:25 +0000

    Adding JS to portfolio

---
+++ code.js
@@ -0,0 +1,11 @@
+const dialog = document.querySelector(".popOut");
+
+const openButton = document.querySelector(".openButton");
+openButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const closeButton = document.querySelector(".closeButton");
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
