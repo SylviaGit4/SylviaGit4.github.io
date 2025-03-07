f91403785e9dbe185356632c950a5a31bcff298c
SylviaGit4 <torchicky139@gmail.com>
2025-03-07 12:00:05 +0000

    JS scripts

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 const titleText = document.querySelector(".title");
 
-title.innerHTML = "Hi";
+titleText.innerHTML = "Test";
+
+
+const dialog = document.querySelector(".popOut");
+
+const openButton = document.querySelector(".openButton");
+openButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const closeButton = document.querySelector(".closeButton");
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
